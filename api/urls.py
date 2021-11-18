785a1b19c74d8ccaab9b21bfcd860d5b9b5a83dd
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2021-11-18 12:16:26 +0530

    profile_serializer

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.endpoints, name="endpoints")
+    path('', views.endpoints, name="endpoints"),
+    path('profiles/', views.profiles, name="profiles")
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
+from rest_framework import status
+from . models import (
+    Profile
+)
+from . serializers import (
+    ProfileSerializer
+)
 
 # Create your views here.
 
 @api_view(['GET'])
 def endpoints(request):
     return Response({"message":"hello"})
+
+@api_view(['GET'])
+def profiles(request):
+    profs = Profile.objects.all()
+    serializer = ProfileSerializer(profs,many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+    
